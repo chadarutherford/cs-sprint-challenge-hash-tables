40a09b2787a227a2841deb004227bb987f753be7
Chad Rutherford <chad_rutherford@outlook.com>
2020-06-04 23:17:08 -0400

    Finished ex3.py
    Tests for test_small don't pass but they work right when I run on main file ????

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,17 +1,25 @@
+cache = {}
+
+
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    result = []
+    for array in arrays:
+        for item in array:
+            if item in cache:
+                cache[item] += 1
+            else:
+                cache[item] = 1
+
+    for key, value in cache.items():
+        if value == len(arrays):
+            result.append(key)
 
     return result
 
-
 if __name__ == "__main__":
-    arrays = []
-
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
-
-    print(intersection(arrays))
+    result = intersection([
+            [1,2,3],
+            [1,4,5],
+            [1,6,7]
+    ])
+    print(result == [1])

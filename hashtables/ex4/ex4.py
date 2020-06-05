1fc927d2059d123e1574e7633de49e24e191b89c
Chad Rutherford <chad_rutherford@outlook.com>
2020-06-04 23:32:46 -0400

    finished ex4.py

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,25 @@
-def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+cache = {}
 
+def has_negatives(a):
+    result = []
+    for (index, item) in enumerate(a):
+        cache[index] = item
+
+    for item in a:
+        if (item * -1) in cache:
+            if item == 0:
+                continue
+            result.append(-item)
     return result
 
-
 if __name__ == "__main__":
-    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+    # print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+    # expected [1, 2, 4]
+
+    a = list(range(5000000))
+    a += [-1,-2,-3]
+
+    result = has_negatives(a)
+    result.sort()
+    print(result)
+    # expected [1, 2, 3]
